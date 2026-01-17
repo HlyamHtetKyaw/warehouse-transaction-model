e5303849863db8ce35b71187607b6eba344bc88f
hlyamhtetkyaw <hlan1559@gmail.com>
2026-01-17 11:24:13 +0630

    [ADD] processing status type

---
+++ doc/model/ProcessingStatusType.java
@@ -0,0 +1,11 @@
+package com.aplusbinary.binarypixor.doc.model;
+
+public enum ProcessingStatusType {
+    PENDING,
+    RESERVED,
+    PROCESSING,
+    SUCCESS,
+    FAILED,
+    INSUFFICIENT_CREDITS,
+    RESERVATION_EXPIRED
+}
